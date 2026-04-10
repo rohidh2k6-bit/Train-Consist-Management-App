b2197d06e0a95e91b9f5a9677a65426b4d20d13e
rohidh2k6-bit <rohidh2k6@gmail.com>
2026-04-10 10:05:12 +0530

    UC2 done

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -2,9 +2,18 @@ import java.util.*;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
-        System.out.println("=== UC1 ===");
+        System.out.println("=== UC2 ===");
 
-        List<String> train = new ArrayList<>();
-        System.out.println("Initial bogie count: " + train.size());
+        List<String> bogies = new ArrayList<>();
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+
+        System.out.println("After add: " + bogies);
+
+        bogies.remove("AC Chair");
+        System.out.println("After remove: " + bogies);
+
+        System.out.println("Contains Sleeper? " + bogies.contains("Sleeper"));
     }
 }
